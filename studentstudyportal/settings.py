7733037bbf65c2128f3a9dd90b0649df296fbf6d
tsonare <tsonare@bestpeers.com>
2022-03-25 11:31:14 +0530

    first Commit at the time of deployment

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -91,14 +91,27 @@ WSGI_APPLICATION = "studentstudyportal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# Database dbsqlite3
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# Settings for Database Postgres
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'studentstudyportal',
+       'USER': 'postgres',
+       'PASSWORD': 'developer',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
